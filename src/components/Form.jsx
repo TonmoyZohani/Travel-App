d5a5e9bcd8651d314a923d36a45ab3c8e2e4729a
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-08-07 01:04:32 +0600

    controlled elements

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("It is clicked...");
+    console.log(description);
+    console.log(quantity);
+
+    const newItem = {
+      id: Date.now(),
+      description: description,
+      quantity: quantity,
+      packed: "false",
+    };
+
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   };
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your ☺ trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(parseInt(e.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
