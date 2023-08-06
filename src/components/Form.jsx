bfd66832083fb53e5f4d5723b2f871727dfa1d45
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-08-06 22:28:41 +0600

    component made

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+
+
+
+  return (
+    <div className='add-form'>
+        <h3>What do you need for your ☺ trip?</h3>
+      
+    </div>
+  )
+}
+
+export default Form
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <h1>
+      🚕 Far Away 🛵
+    </h1>
+  )
+}
+
+export default Logo
