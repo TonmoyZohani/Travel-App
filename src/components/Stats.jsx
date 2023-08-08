8f92d96456e8db38a1b829e34a35b16be76e4cba
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-08-08 22:44:01 +0600

    statistics done

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
 const Stats = ({ items }) => {
+  if (items.length === 0) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+  }
+
   const packed = items.filter((item) => item.packed === true).length;
-
   const percentage = Math.round((packed / items.length) * 100);
-
-  console.log(items);
 
   return (
     <footer className="stats">
-      <em>
-        You have {items.length} items on your list, and you already packed{" "}
-        {packed}({percentage}%)
-      </em>
+      {percentage === 100 ? (
+        <em>You have packed everything.Ready to go</em>
+      ) : (
+        <em>
+          You have {items.length} items on your list, and you already packed{" "}
+          {packed}({percentage}%)
+        </em>
+      )}
     </footer>
   );
 };
